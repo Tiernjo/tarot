19855844f219af399d7f0710c4f6cbc9c0da4866
takuin <takuin@mint>
2014-02-07 13:31:19 -0600

    added font and reorganized tarot card images

--- src/control.rs
+++ src/control.rs
@@ -1,8 +1,9 @@
 extern mod rsfml;
-use rsfml::window::{event};
+use rsfml::window::{event, keyboard};
 use rsfml::graphics::{RenderWindow};
 
 pub fn exit(window: &mut RenderWindow) {
+	if keyboard::is_key_pressed(keyboard::Escape) {window.close()}
 	loop {
 		match window.poll_event() {
 			event::Closed	=> window.close(),
